95c177c3dd1b6f0520f001d9949fb7459e6dcef2
siva-kannan3 <sivakwalker3@gmail.com>
2026-09-25 06:54:31 +0530

    feat: graceful shutdown

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"ride-sharing/shared/env"
 )
@@ -12,33 +17,43 @@ var (
 )
 
 func main() {
-	log.Println("Starting API Gateway")
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /trip/preview", handleTripPreview)
 
-	// mux.HandleFunc("GET /trip/preview", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.WriteHeader(http.StatusOK)
-	// 	w.Write([]byte("Hello from API Gateway"))
-	// })
-
-	// mux.HandleFunc("GET /trip/preview", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.WriteHeader(http.StatusOK)
-	// 	w.Write([]byte("Hello from API Gateway"))
-	// })
-
-	// mux.HandleFunc("GET /trip/preview", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.WriteHeader(http.StatusOK)
-	// 	w.Write([]byte("Hello from API Gateway"))
-	// })
-
 	server := &http.Server{
-		Addr:    httpAddr,
-		Handler: mux,
+		Addr:         httpAddr,
+		Handler:      mux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  60 * time.Second,
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Printf("HTTP server error: %v", err)
+	serverErrors := make(chan error, 1)
+
+	go (func() {
+		log.Printf("Server is listening on PORT: %s", httpAddr)
+		if err := server.ListenAndServe(); err != nil {
+			serverErrors <- err
+		}
+	})()
+
+	shutdownSignal := make(chan os.Signal, 1)
+	signal.Notify(shutdownSignal, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("Go server failed to start: %v", err)
+	case signal := <-shutdownSignal:
+		log.Fatalf("Shutdown signal received: %s", signal)
+	}
+
+	shutdownContext, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownContext); err != nil {
+		server.Close()
+		log.Printf("Graceful shutdown failed: %v", err)
 	}
 }
